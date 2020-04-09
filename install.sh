7f69d56167fe9a8c43cbecbde471a607ac4c863a
Sebastão Giacheto Ferreira Júnior <sebastorama@gmail.com>
2020-04-09 01:16:42 -0400

    Adds nvim config

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 mkdir -p ~/.vim/tmp
 mkdir -p ~/.tmux/themes
+mkdir -p ~/.config/nvim/plugged
 ln -nsf ~/.dotfiles/gitconfig ~/.gitconfig
 ln -nsf ~/.dotfiles/githelpers ~/.githelpers
 ln -nsf ~/.dotfiles/vimrc ~/.vimrc
 ln -nsf ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -nsf ~/.dotfiles/tomorrow.tmux ~/.tmux/themes/tomorrow.tmux
+ln -nsf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
