6e2c49c0b9963819dd62d72be34829edba5b3c85
Sebastão Giacheto Ferreira Júnior <sebastorama@gmail.com>
2020-06-21 02:35:53 -0400

    Almost a complete setup!

--- install.sh
+++ install.sh
@@ -11,7 +11,9 @@ ln -nsf ~/.dotfiles/tomorrow.tmux ~/.tmux/themes/tomorrow.tmux
 [ -d "$HOME/.config/i3" ] && [ ! -L "$HOME/.config/i3" ] && rm -rf ~/.config/i3
 [ -d "$HOME/.config/alacritty" ] && [ ! -L "$HOME/.config/alacritty" ] && rm -rf ~/.config/alacritty
 [ -d "$HOME/.config/polybar" ] && [ ! -L "$HOME/.config/polybar" ] && rm -rf ~/.config/polybar
+[ -d "$HOME/.config/rofi" ] && [ ! -L "$HOME/.config/rofi" ] && rm -rf ~/.config/rofi
 ln -nsf ~/.dotfiles/.config/nvim ~/.config/nvim
 ln -nsf ~/.dotfiles/.config/i3 ~/.config/i3
 ln -nsf ~/.dotfiles/.config/alacritty ~/.config/alacritty
 ln -nsf ~/.dotfiles/.config/polybar ~/.config/polybar
+ln -nsf ~/.dotfiles/.config/rofi ~/.config/rofi
