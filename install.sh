ffdd503de952ad6a02ea415c023a154c196f1a23
Sebastão Giacheto Ferreira Júnior <sebastorama@gmail.com>
2020-06-20 23:06:54 -0400

    Tons of new stuff

--- install.sh
+++ install.sh
@@ -6,4 +6,12 @@ ln -nsf ~/.dotfiles/githelpers ~/.githelpers
 ln -nsf ~/.dotfiles/vimrc ~/.vimrc
 ln -nsf ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -nsf ~/.dotfiles/tomorrow.tmux ~/.tmux/themes/tomorrow.tmux
-ln -nsf ~/.dotfiles/nvim ~/.config/nvim
+
+[ -d "$HOME/.config/nvim" ] && [ ! -L "$HOME/.config/nvim" ] && rm -rf ~/.config/nvim
+[ -d "$HOME/.config/i3" ] && [ ! -L "$HOME/.config/i3" ] && rm -rf ~/.config/i3
+[ -d "$HOME/.config/alacritty" ] && [ ! -L "$HOME/.config/alacritty" ] && rm -rf ~/.config/alacritty
+[ -d "$HOME/.config/polybar" ] && [ ! -L "$HOME/.config/polybar" ] && rm -rf ~/.config/polybar
+ln -nsf ~/.dotfiles/.config/nvim ~/.config/nvim
+ln -nsf ~/.dotfiles/.config/i3 ~/.config/i3
+ln -nsf ~/.dotfiles/.config/alacritty ~/.config/alacritty
+ln -nsf ~/.dotfiles/.config/polybar ~/.config/polybar
